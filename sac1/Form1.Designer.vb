0ca1a28e2f458f5a40681cc2d46c96a2ff60fdbe
Archit Venkita Subharaman <venki15046@students.pvcc.vic.edu.au>
2022-03-07 12:23:20 +1100

    Add project files.

---
+++ sac1/Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(105, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "JOHN"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(94, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "DAMIEN"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(105, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "MARK"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(94, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "JOSHUA"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(105, 170)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "MARY"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(105, 198)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "MARIA"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(150, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(150, 77)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(150, 109)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(150, 136)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(150, 167)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox5.TabIndex = 10
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(150, 198)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox6.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(512, 44)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 29)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "English Marks"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(512, 79)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 36)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Maths Marks "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(512, 132)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(120, 23)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "PE marks "
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(512, 160)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(120, 30)
+        Me.Button4.TabIndex = 16
+        Me.Button4.Text = "Reset marks "
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(56, 310)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(117, 30)
+        Me.Button5.TabIndex = 17
+        Me.Button5.Text = "add to csv file"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(56, 346)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(117, 39)
+        Me.Button6.TabIndex = 18
+        Me.Button6.Text = "Read from csv file"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(213, 319)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Label7"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(150, 12)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox7.TabIndex = 20
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(150, 233)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(300, 20)
+        Me.TextBox8.TabIndex = 21
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(24, 236)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(122, 13)
+        Me.Label8.TabIndex = 22
+        Me.Label8.Text = "Average for the subjects"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ sac1/Form1.vb
@@ -0,0 +1,200 @@
+﻿Public Class Form1
+    Dim english As Integer
+    Dim maths As Integer
+    Dim PE As Integer
+    Dim average As Integer
+    Dim enav As Integer
+    Dim maav As Integer
+    Dim peav As Integer
+
+    Dim english1 As Integer
+    Dim maths1 As Integer
+    Dim PE1 As Integer
+    Dim average1 As Integer
+
+    Dim english2 As Integer
+    Dim maths2 As Integer
+    Dim PE2 As Integer
+    Dim average2 As Integer
+
+    Dim english3 As Integer
+    Dim maths3 As Integer
+    Dim PE3 As Integer
+    Dim averag3 As Integer
+
+    Dim english4 As Integer
+    Dim maths4 As Integer
+    Dim PE4 As Integer
+    Dim average4 As Integer
+
+    Dim english5 As Integer
+    Dim maths5 As Integer
+    Dim PE5 As Integer
+    Dim average5 As Integer
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        AddHandler Button1.Click, AddressOf MyClickHandler
+        AddHandler Button2.Click, AddressOf MyClickHandler
+        AddHandler Button3.Click, AddressOf MyClickHandler
+        AddHandler Button4.Click, AddressOf MyClickHandler
+        AddHandler Button5.Click, AddressOf MyClickHandler
+        AddHandler Button6.Click, AddressOf MyClickHandler
+        MsgBox("please enter fresh values to get the averages per subjects")
+
+        TextBox7.Text = ("ENGLISH" & "          " & "MATHS" & "          " & "PE" & "          " & "AVERAGE")
+    End Sub
+
+    Private Sub MyClickHandler(sender As Object, e As EventArgs)
+
+
+        Select Case CType(sender, Button).Name
+            Case "Button1"
+                If english = 0 Then
+
+                    english = InputBox("How many marks would you like to award for John's english")
+                    TextBox1.Text = (english & "                      " & maths & "                      " & PE)
+
+                    english1 = InputBox("How many marks would you like to award for Damein's english")
+                    TextBox2.Text = (english1 & "                      " & maths1 & "                     " & PE1)
+
+                    english2 = InputBox("How many marks would you like to award for Mark's english")
+                    TextBox3.Text = (english2 & "                      " & maths2 & "                     " & PE2)
+
+                    english3 = InputBox("How many marks would you like to award for Joshua's english")
+                    TextBox4.Text = (english3 & "                      " & maths3 & "                     " & PE3)
+
+                    english4 = InputBox("How many marks would you like to award for Mary's english")
+                    TextBox5.Text = (english4 & "                      " & maths4 & "                     " & PE4)
+
+                    english5 = InputBox("How many marks would you like to award for Maria's english")
+                    TextBox6.Text = (english5 & "                      " & maths5 & "                     " & PE5)
+                    enav = (english + english1 + english2 + english3 + english4 + english5) / 3
+                    TextBox8.Text = (enav)
+                Else
+                    MsgBox("Please click the reset button to reset the marks.")
+                End If
+
+            Case "Button2"
+                If maths = 0 Then
+
+                    maths = InputBox("How many marks would you like to award for John's maths")
+                    TextBox1.Text = (english & "                      " & maths & "                      " & PE)
+
+                    maths1 = InputBox("How many marks would you like to award for Damein's maths")
+                    TextBox2.Text = (english1 & "                      " & maths1 & "                      " & PE1)
+
+                    maths2 = InputBox("How many marks would you like to award for Mark's maths")
+                    TextBox3.Text = (english2 & "                      " & maths2 & "                      " & PE2)
+
+                    maths3 = InputBox("How many marks would you like to award for Joshua's maths")
+                    TextBox4.Text = (english3 & "                      " & maths3 & "                      " & PE3)
+
+                    maths4 = InputBox("How many marks would you like to award for Mary's maths")
+                    TextBox5.Text = (english4 & "                      " & maths4 & "                      " & PE4)
+
+                    maths5 = InputBox("How many marks would you like to award for Maria's maths")
+                    TextBox6.Text = (english5 & "                      " & maths5 & "                      " & PE5)
+                    maav = (maths + maths1 + maths2 + maths3 + maths4 + maths5) / 3
+                    TextBox8.Text = (enav & "                      " & maav)
+                Else
+                    MsgBox("Please click the reset button to reset the marks.")
+                End If
+
+            Case "Button3"
+                If PE = 0 Then
+
+                    PE = InputBox("How many marks would you like to award for John's PE")
+                    average = (english + maths + PE) / 3
+                    TextBox1.Text = (english & "                      " & maths & "                      " & PE & "  " & average)
+
+                    PE1 = InputBox("How many marks would you like to award for Damein's PE")
+                    average1 = (english1 + maths1 + PE1) / 3
+                    TextBox2.Text = (english1 & "                       " & maths1 & "                      " & PE1 & "  " & average1)
+
+                    PE2 = InputBox("How many marks would you like to award for Mark's PE")
+                    average2 = (english2 + maths2 + PE2) / 3
+                    TextBox3.Text = (english2 & "                      " & maths2 & "                       " & PE2 & "  " & average2)
+
+                    PE3 = InputBox("How many marks would you like to award for Joshua's PE")
+                    averag3 = (english3 + maths3 + PE3) / 3
+                    TextBox4.Text = (english3 & "                      " & maths3 & "                       " & PE3 & "  " & averag3)
+
+                    PE4 = InputBox("How many marks would you like to award for Mary's PE")
+                    average4 = (english4 + maths4 + PE4) / 3
+                    TextBox5.Text = (english4 & "                      " & maths4 & "                       " & PE4 & "  " & average4)
+
+                    PE5 = InputBox("How many marks would you like to award for Maria's PE")
+                    average5 = (english5 + maths5 + PE5) / 3
+
+                    peav = (PE + PE1 + PE2 + PE3 + PE4 + PE5) / 3
+                    TextBox8.Text = (enav & "                      " & maav & "                      " & peav)
+
+                    TextBox6.Text = (english5 & "                      " & maths5 & "                       " & PE5 & "  " & average5)
+
+                Else
+                    MsgBox("Please click the reset button to reset the marks.")
+
+                End If
+
+            Case "Button4"
+
+                maths = 0
+                maths1 = 0
+                maths2 = 0
+                maths3 = 0
+                maths4 = 0
+                maths5 = 0
+
+                english = 0
+                english1 = 0
+                english2 = 0
+                english3 = 0
+                english4 = 0
+                english5 = 0
+
+                PE = 0
+                PE1 = 0
+                PE2 = 0
+                PE3 = 0
+                PE4 = 0
+                PE5 = 0
+
+                TextBox1.Text = (english & "                       " & maths & "                       " & PE)
+                TextBox2.Text = (english1 & "                       " & maths1 & "                       " & PE1)
+                TextBox3.Text = (english2 & "                       " & maths2 & "                       " & PE2)
+                TextBox4.Text = (english3 & "                       " & maths3 & "                       " & PE3)
+                TextBox5.Text = (english4 & "                       " & maths4 & "                       " & PE4)
+                TextBox6.Text = (english5 & "                       " & maths5 & "                       " & PE5)
+
+            Case "Button5"
+                Dim FileNum As Integer = FreeFile()
+                FileOpen(FileNum, "E:\markdata.csv", OpenMode.Output)
+                PrintLine(FileNum, "NAME,ENGLISH,MATH,PE, AVERAGE")
+                PrintLine(FileNum, "John," & english & "," & maths & "," & PE & "," & average)
+                PrintLine(FileNum, "Damein," & english1 & "," & maths1 & "," & PE1 & "," & average1)
+                PrintLine(FileNum, "Mark," & english2 & "," & maths2 & "," & PE2 & "," & average2)
+                PrintLine(FileNum, "Joshua," & english3 & "," & maths3 & "," & PE3 & "," & averag3)
+                PrintLine(FileNum, "Mary," & english4 & "," & maths4 & "," & PE4 & "," & average4)
+                PrintLine(FileNum, "Maria," & english5 & "," & maths5 & "," & PE5 & "," & average5)
+                PrintLine(FileNum, "average for the subjects" & enav & "," & maav & "," & peav)
+                FileClose(FileNum)
+
+            Case "Button6"
+                Dim FileNum As Integer = FreeFile()
+                Dim TempS As String = ""
+                Dim Templ As String
+                FileOpen(FileNum, "E:\markdata.csv", OpenMode.Input)
+                Do Until EOF(FileNum)
+                    Templ = LineInput(FileNum)
+                    TempS += Templ + vbCrLf
+                Loop
+                FileClose(FileNum)
+                Label7.Text = TempS
+        End Select
+
+    End Sub
+
+End Class
